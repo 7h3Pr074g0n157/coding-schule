96f7aa9a638a91ec23fe04b8e62a1bcfc12f3ac6
n8trufe <n8trufe@gmail.com>
2019-11-03 19:52:07 +0100

    Add template-card and replace templates from json

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const fs = require('fs');
 const form = fs.readFileSync('./neue-kurse.html', 'utf-8');
 let courseList = fs.readFileSync('./kurs-liste.json');
 courseList = JSON.parse(courseList);
+let tempCard = fs.readFileSync('./template-card.html').toString();
 let courseCards = fs.readFileSync('./kurs-karten.html').toString();
+
+function replaceTemplates(tempCard, courseCard) {
+  let output = tempCard.replace(/{%NAME%}/g, courseCard.name);
+  output = output.replace(/{%BESCHREIBUNG%}/g, courseCard.beschreibung);
+  return output;
+}
 
 const server = http.createServer((req, res) => {
   const url = req.url,
@@ -30,18 +37,21 @@ const server = http.createServer((req, res) => {
           .join(' ')
       };
       courseList.push(newCourse);
-      // console.log(courseList);
+
       courseList = JSON.stringify(courseList);
       fs.writeFileSync('./kurs-liste.json', courseList);
     });
     res.writeHead(302, { 'Content-type': 'text/html', Encoding: 'utf-8' });
-console.log(courseCards);
-    courseCards = courseCards.replace('Kursname', courseList[0].name);
-    courseCards = courseCards.replace(
-      'Kursbeschreibung',
-      courseList[0].beschreibung
-    );
-console.log(courseCards);
+
+    let output = courseList
+      .map(el => {
+        return replaceTemplates(tempCard, el);
+      })
+      .toString();
+
+    courseCards = courseCards.replace(/{%CARDS%}/, output);
+    console.log(courseCards);
+
     res.write(courseCards);
 
     return res.end();
